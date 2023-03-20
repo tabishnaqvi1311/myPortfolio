a470d2a2b1c23a7071688677eff6faff0ebedfa2
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-03-20 23:56:40 +0530

    initialized using create-react-app

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        background: 'black',
+        accents: '#008080',
+        other: '#015c5c'
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
