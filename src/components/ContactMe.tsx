a26a0c187c9a5db3ab2eda0820aa70755e70ea81
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-10-03 21:57:00 +0530

    fixed bug in footer

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -7,7 +7,7 @@ export const ContactMe = () => {
         <section id="contact" className="relative">
 
             <div className="absolute bottom-1/2 right-1/4 w-48 h-48 bg-lime-300 bg-opacity-10 rounded-full"></div>
-            <div className="absolute top-1/3 right-1/3 w-60 h-60 bg-gray-200 bg-opacity-5 rounded-full"></div>
+            <div className="absolute top-1/2 right-1/3 w-60 h-60 bg-gray-200 bg-opacity-5 rounded-full"></div>
             <motion.div className="flex lg:flex-row flex-col justify-center items-center lg:gap-40 gap-20 lg:py-40 py-20" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.5 }}>
                 <div>
                     <span className="text-[#83cb15]">Contact Me</span>
