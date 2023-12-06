6de0040309e8b2c1b738408717757ed3a9893762
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-12-06 21:12:54 +0530

    updated projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,10 +14,10 @@ export const Projects = () => {
     const cardDetails: CardDetails[] = [
         {
             id: 1,
-            name: "CulinaryCrave",
-            desc: "A recipe webite with fully functional CRUD operations and authentication using jwt",
-            techUsed: ["react", "tailwind", "mongodb"],
-            linkToProject: "https://github.com/tabishnaqvi1311/CulinaryCrave"
+            name: "Enliven",
+            desc: "A mental health platform for underpriviliged youth built in Next.js",
+            techUsed: ["nextjs", "openai", "shadcn"],
+            linkToProject: "https://github.com/tabishnaqvi1311/enliven-sih"
 
         },
         {
@@ -31,9 +31,16 @@ export const Projects = () => {
             id: 3,
             name: "Twitter Bot",
             desc: "Bot that posts memes on twitter built using Tweepy library",
-            techUsed: ["twitter", "Twiiter API", "javascript"],
+            techUsed: ["twitter", "Twiter API", "python"],
             linkToProject: "https://github.com/tabishnaqvi1311/twitter-bot"
         },
+        {
+            id: 4,
+            name: "CulinaryCrave",
+            desc: "A recipe webite with fully functional CRUD operations and authentication using jwt",
+            techUsed: ["react", "tailwind", "mongodb"],
+            linkToProject: "https://github.com/tabishnaqvi1311/CulinaryCrave"
+        }
 
     ]
 
@@ -43,8 +50,8 @@ export const Projects = () => {
                 <span className="uppercase text-[#84cc14]">My Works</span>
                 <motion.h1 className="text-5xl font-bold" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.4 }}>Projects I've Made</motion.h1>
             </div>
-            <div className="flex justify-center items-center p-20">
-                <motion.div className="flex lg:flex-row flex-col gap-40" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.7 }}>
+            <div className="flex justify-center items-center lg:p-20 p-5">
+                <motion.div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-40 gap-10" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.7 }}>
                     {cardDetails.map((card) => (
                         <Card name={card.name} desc={card.desc} techUsed={card.techUsed} linkToProject={card.linkToProject} key={card.id}/>
                     ))}
