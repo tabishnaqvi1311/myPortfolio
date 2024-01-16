2adc25c21edf4f6791574866d77e1c78f5c07a31
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2024-01-16 23:34:21 +0530

    updated resume link and other changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,51 +5,51 @@ import laptop from "../assets/laptop.png"
 
 export const Hero = () => {
 
-    const resumeLink = "https://drive.google.com/file/d/1amLmPEQd79KDhauzyd7i32ZcNrt5itaF/view?usp=drive_link";
+  const resumeLink = "https://drive.google.com/file/d/1w0O-qGH05H1-MdpXvE3y_4qK1s6kmNll/view?usp=sharing";
 
-    return (
-        <motion.section className=" h-screen `" id="home" initial={{ opacity: '0' }} animate={{ opacity: 100 }} transition={{ delay: 0.5 }}>
-            <div className="absolute -top-12 left-0 w-40 h-40 bg-lime-500 bg-opacity-10 rounded-full"></div>
-            <div className="absolute top-1/2 right-1/4 w-48 h-48 bg-lime-300 bg-opacity-10 rounded-full"></div>
-            <div className="absolute top-1/3 right-1/3 w-60 h-60 bg-gray-200 bg-opacity-5 rounded-full"></div>
+  return (
+    <motion.section className=" h-screen `" id="home" initial={{ opacity: '0' }} animate={{ opacity: 100 }} transition={{ delay: 0.5 }}>
+      <div className="absolute -top-12 left-0 w-40 h-40 bg-lime-500 bg-opacity-10 rounded-full"></div>
+      <div className="absolute top-1/2 right-1/4 w-48 h-48 bg-lime-300 bg-opacity-10 rounded-full"></div>
+      <div className="absolute top-1/3 right-1/3 w-60 h-60 bg-gray-200 bg-opacity-5 rounded-full"></div>
 
-            <div className="flex lg:flex-row flex-col items-center justify-evenly lg:py-40 p-10 text-center lg:text-start ">
-                <div className="flex flex-col gap-8 mb-10">
-                    <h1 className="lg:text-7xl text-5xl font-black">Hey, My name is <br /><span className="text-lime-500">Tabish</span> Naqvi.</h1>
-                    <p className="text-lg">A 20-year-old self-made full stack developer <br />with passion for problem-solving using code.</p>
-                    <div className="flex gap-5 items-center text-sm lg:text-base ">
-                        <a href="https://gunhawke.hashnode.dev/" className="flex items-center gap-2 hover:text-lime-500 transition-all duration-200" target="_blank">Read the blog</a>
-                        <span>|</span>
-                        <a href="https://moyaai.tech" className="flex items-center gap-2 text-lime-500 transition-all duration-200" target="_blank">
-                            Check Out Moyaai
-                        </a>
-                    </div>
-                    <a href={resumeLink} target="_blank">
-                        <Button content="R E S U M E" />
-                    </a>
-                </div>
-                <div>
-                    <img src={laptop} alt="a laptop" />
-                </div>
+      <div className="flex lg:flex-row flex-col items-center justify-evenly lg:py-40 p-10 text-center lg:text-start ">
+        <div className="flex flex-col gap-8 mb-10">
+          <h1 className="lg:text-7xl text-5xl font-black">Hey, My name is <br /><span className="text-lime-500">Tabish</span> Naqvi.</h1>
+          <p className="text-lg">A 20-year-old self-made full stack developer <br />with passion for problem-solving using code.</p>
+          <div className="flex gap-5 items-center text-sm lg:text-base ">
+            <a href="https://gunhawke.hashnode.dev/" className="flex items-center gap-2 hover:text-lime-500 transition-all duration-200" target="_blank">Read the blog</a>
+            <span>|</span>
+            <a href="https://moyaai.tech" className="flex items-center gap-2 text-lime-500 transition-all duration-200" target="_blank">
+              Check Out Moyaai
+            </a>
+          </div>
+          <a href={resumeLink} target="_blank">
+            <Button content="R E S U M E" />
+          </a>
+        </div>
+        <div>
+          <img src={laptop} alt="a laptop" />
+        </div>
 
-                <div className='absolute xs:bottom-10 bottom-10 w-full lg:flex hidden justify-center items-center'>
-                    <a href='#about'>
-                        <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-                            <motion.div
-                                animate={{
-                                    y: [0, 24, 0],
-                                }}
-                                transition={{
-                                    duration: 1.5,
-                                    repeat: Infinity,
-                                    repeatType: "loop",
-                                }}
-                                className='w-3 h-3 rounded-full bg-white mb-1'
-                            />
-                        </div>
-                    </a>
-                </div>
+        <div className='absolute xs:bottom-10 bottom-10 w-full lg:flex hidden justify-center items-center'>
+          <a href='#about'>
+            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className='w-3 h-3 rounded-full bg-white mb-1'
+              />
             </div>
-        </motion.section>
-    )
+          </a>
+        </div>
+      </div>
+    </motion.section>
+  )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,75 +1,75 @@
 import { Card } from "./Card";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 type CardDetails = {
-    id: number,
-    name: string,
-    desc: string,
-    techUsed: string[],
-    linkToProject: string
+  id: number,
+  name: string,
+  desc: string,
+  techUsed: string[],
+  linkToProject: string
 }
 
 export const Projects = () => {
 
-    const cardDetails: CardDetails[] = [
-        {
-            id: 1,
-            name: "Moyaai",
-            desc: "A job matching platform for Youtubers and job seekers that puts community building first",
-            techUsed: ["youtubeAPI", "mongodb", "react"],
-            linkToProject: "https://moyaai.tech"
-        },
-        {
-            id: 2,
-            name: "Attendance Notifier",
-            desc: "Discord bot to notify college attendance on lazy mornings!",
-            techUsed: ["puppeteer", "discordAPI", "node"],
-            linkToProject: "https://github.com/tabishnaqvi1311/erp-crawler"
-        },
-        {
-            id: 3,
-            name: "Twitter Bot",
-            desc: "Bot that posts memes on twitter built using Tweepy library",
-            techUsed: ["twitter", "Twiter API", "python"],
-            linkToProject: "https://github.com/tabishnaqvi1311/twitter-bot"
-        },
-        {
-            id: 4,
-            name: "CulinaryCrave",
-            desc: "A recipe webite built with the Spoonacular API",
-            techUsed: ["react", "tailwind", "spoonacular"],
-            linkToProject: "https://github.com/tabishnaqvi1311/CulinaryCrave"
-        },
-        {
-            id: 5,
-            name: "3-D Platformer",
-            desc: "A 3D platformer game built using Unity and C#",
-            techUsed: ["unity", "c#", "blender"],
-            linkToProject: "https://github.com/tabishnaqvi1311/3dGame"
-        },
-        {
-            id: 6,
-            name: "Enliven",
-            desc: "A mental health ecosystem (website+game) for children built in Next.js",
-            techUsed: ["c#", "unity", "nextjs"],
-            linkToProject: "https://github.com/tabishnaqvi1311/enliven-sih"
+  const cardDetails: CardDetails[] = [
+    {
+      id: 1,
+      name: "Moyaai",
+      desc: "A job matching platform for Youtubers and job seekers that puts community building first",
+      techUsed: ["youtubeAPI", "mongodb", "react"],
+      linkToProject: "https://moyaai.tech"
+    },
+    {
+      id: 2,
+      name: "Attendance Notifier",
+      desc: "Discord bot to notify college attendance on lazy mornings!",
+      techUsed: ["puppeteer", "discordAPI", "node"],
+      linkToProject: "https://github.com/tabishnaqvi1311/erp-crawler"
+    },
+    {
+      id: 3,
+      name: "Twitter Bot",
+      desc: "Bot that posts memes on twitter built using Tweepy library",
+      techUsed: ["twitter", "Twiter API", "python"],
+      linkToProject: "https://github.com/tabishnaqvi1311/twitter-bot"
+    },
+    {
+      id: 4,
+      name: "CulinaryCrave",
+      desc: "A recipe webite built with the Spoonacular API",
+      techUsed: ["react", "tailwind", "spoonacular"],
+      linkToProject: "https://github.com/tabishnaqvi1311/CulinaryCrave"
+    },
+    {
+      id: 5,
+      name: "3-D Platformer",
+      desc: "A 3D platformer game built using Unity and C#",
+      techUsed: ["unity", "c#", "blender"],
+      linkToProject: "https://github.com/tabishnaqvi1311/3dGame"
+    },
+    {
+      id: 6,
+      name: "Enliven",
+      desc: "A mental health tracker (website+game) for children",
+      techUsed: ["c#", "unity", "nextjs"],
+      linkToProject: "https://github.com/tabishnaqvi1311/enliven-sih"
 
-        },
-    ]
+    },
+  ]
 
-    return (
-        <div className="p-10" id="projects">
-            <div className="">
-                <span className="uppercase text-[#84cc14]">My Works</span>
-                <motion.h1 className="text-5xl font-bold" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.4 }}>Projects I've Made</motion.h1>
-            </div>
-            <div className="flex justify-center items-center lg:p-20 p-5">
-                <motion.div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-40 gap-10" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.7 }}>
-                    {cardDetails.map((card) => (
-                        <Card name={card.name} desc={card.desc} techUsed={card.techUsed} linkToProject={card.linkToProject} key={card.id}/>
-                    ))}
-                </motion.div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="p-10" id="projects">
+      <div className="">
+        <span className="uppercase text-[#84cc14]">My Works</span>
+        <motion.h1 className="text-5xl font-bold" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.4 }}>Projects I've Made</motion.h1>
+      </div>
+      <div className="flex justify-center items-center lg:p-20 p-5">
+        <motion.div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-40 gap-10" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.7 }}>
+          {cardDetails.map((card) => (
+            <Card name={card.name} desc={card.desc} techUsed={card.techUsed} linkToProject={card.linkToProject} key={card.id} />
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  )
 }
