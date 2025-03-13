ad935ae568d7133bb8a7ec5f8425ae481b466381
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-03-13 14:31:30 +0530

    made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Hero } from "./components/Hero"
 import { Navbar } from "./components/Navbar"
 import { Projects } from "./components/Projects"
 import { Skills } from "./components/Skills"
-import { Timeline } from "./components/Timeline"
+// import { Timeline } from "./components/Timeline"
 import Footer from "./components/Footer"
 import CodingActivity from "./components/CodingActivity"
 import StarPrompt from "./components/StarPrompt"
@@ -27,9 +27,9 @@ const App = () => {
           <div className="main">
             <Navbar />
             <Hero />
-            <Timeline />
-            <Projects />
+            {/* <Timeline /> */}
             <CodingActivity />
+            <Projects />
             <Skills />
             {/* <Blogs/> */}
             <ContactMe />
