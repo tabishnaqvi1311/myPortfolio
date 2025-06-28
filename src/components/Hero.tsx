6c56d62da93dbbfbab3bc4054c3f76d8d09e7caa
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-06-28 18:25:08 +0530

    added content to hero, moved resume button to navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,53 +1,68 @@
-import "../styles/Hero.css"
-import { Button } from "./Button"
-import { motion } from "framer-motion"
-import { useScramble } from "use-scramble"
+import "../styles/Hero.css";
+import { motion } from "framer-motion";
+import { useScramble } from "use-scramble";
 
 export const Hero = () => {
-
-  const {ref, replay} = useScramble({
-    text: "Tabish Naqvi",
-    speed: 0.9,
-    tick: 2,
-    step: 1,
-    scramble: 3,
-    seed: 0,
-    overdrive: false 
-  })
-
-  const resumeLink = "https://drive.google.com/file/d/1LJkzJuSLBxuWwZe3GFoYBxS0H2jmsU7G/view?usp=drive_link";
-
-  return (
-    <motion.section className=" h-screen `" id="home" initial={{ opacity: '0' }} animate={{ opacity: 100 }} transition={{ delay: 0.1 }}>
-
-      <div className="flex lg:flex-row flex-col items-center justify-evenly lg:py-40 p-10 text-center lg:text-start ">
-        <div className="flex flex-col items-center gap-8 mb-10">
-          <h1 className="lg:text-7xl text-5xl text-center poppins-medium" onMouseOver={replay}>Hey, My name is <br />
-          <span className="text-lime-500" ref={ref}>Tabish Naqvi.</span></h1>
-          <p className="text-lg text-center space-grotesk-400 text-gray-300">I'm a full stack developer who's into tech and startups. <br /> I love building things and am always open to learn</p>
-          <a href={resumeLink} target="_blank">
-            <Button content="R E S U M E" />
-          </a>
-        </div>
-
-        <div className='absolute xs:bottom-10 bottom-10 w-full lg:flex hidden justify-center items-center'>
-          <a href='#about'>
-            <div className='w-[30px] h-[60px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-              <motion.div
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 3.0,
-                  repeat: Infinity,
-                  repeatType: "loop",
-                }}
-                className='w-3 h-3 rounded-full bg-white mb-1'
-              />
+    const { ref, replay } = useScramble({
+        text: "Tabish Naqvi",
+        speed: 0.9,
+        tick: 2,
+        step: 1,
+        scramble: 3,
+        seed: 0,
+        overdrive: false,
+    });
+    return (
+        <motion.section
+            className="h-screen"
+            id="home"
+            initial={{ opacity: "0" }}
+            animate={{ opacity: 100 }}
+            transition={{ delay: 0.1 }}
+        >
+            <div className="flex md:flex-row flex-col items-center justify-evenly md:py-24 md:px-10 px-5 text-center">
+                <div className="flex flex-col items-center gap-8 mb-10">
+                    <h1
+                        className="lg:text-7xl text-5xl text-left poppins-medium w-3/4"
+                        onMouseOver={replay}
+                    >
+                        Hey, My name is <br />
+                        <span className="text-lime-500" ref={ref}>
+                            Tabish Naqvi.
+                        </span>
+                    </h1>
+                    <p className="text-lg text-left space-grotesk-400 text-gray-300 w-3/4">
+                        I'm a full-stack developer familiar with JS/TS
+                        ecosystems, with additional experience writing Go and
+                        Java.
+                        <br />
+                        <br /> I've delivered multiple freelance projects,
+                        interned at a startup, and failed to build my own once.{" "}
+                        <br />
+                        <br /> I'm open to work or collaborate,{" "}
+                        <a href="#contact" className="underline text-lime-500">
+                            contact me
+                        </a>
+                    </p>
+                </div>
+                <div className="absolute xs:bottom-10 bottom-10 w-full md:flex hidden justify-center items-center">
+                    <a href="#about">
+                        <div className="w-[30px] h-[60px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                            <motion.div
+                                animate={{
+                                    y: [0, 24, 0],
+                                }}
+                                transition={{
+                                    duration: 3.0,
+                                    repeat: Infinity,
+                                    repeatType: "loop",
+                                }}
+                                className="w-3 h-3 rounded-full bg-white mb-1"
+                            />
+                        </div>
+                    </a>
+                </div>
             </div>
-          </a>
-        </div>
-      </div>
-    </motion.section>
-  )
-}
+        </motion.section>
+    );
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,80 +1,97 @@
 import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi"
-import { CgClose } from "react-icons/cg"
-import { motion } from "framer-motion"
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
+import { motion } from "framer-motion";
 import NavLink from "./NavLink";
 
 type NavLink = {
-    id: number,
-    title: string
-    link: string
-}
+    id: number;
+    title: string;
+    link: string;
+};
 
 export const Navbar = () => {
-
-
-    const [toggle, setToggle] = useState<Boolean>(false);
-
-
+    const [toggle, setToggle] = useState<boolean>(false);
 
     const navLinks: NavLink[] = [
         {
             id: 3,
-            title: 'Github',
-            link: "https://github.com/tabishnaqvi1311"
+            title: "Github",
+            link: "https://github.com/tabishnaqvi1311",
         },
         {
             id: 2,
             title: "Blog",
-            link: "https://gunhawke.hashnode.dev/"
+            link: "https://gunhawke.hashnode.dev/",
         },
         {
             id: 1,
             title: "Contact",
-            link: "mailto:tabish.naqvi2003@gmail.com"
+            link: "mailto:tabish.naqvi2003@gmail.com",
         },
-
+        {
+            id: 69,
+            title: "Resume",
+            link: "https://drive.google.com/file/d/1LJkzJuSLBxuWwZe3GFoYBxS0H2jmsU7G/view?usp=drive_link",
+        },
     ];
 
-
     return (
-        <div className='bricolage-grotesque-300'>
+        <div className="bricolage-grotesque-300">
             {/* for large devices */}
             <nav className="flex justify-between items-center md:p-4 p-2">
                 <div className="px-7 text-3xl">
                     <span className="text-lime-500">{"<"}</span>
-                    {'t/'}
+                    {"t/"}
                     <span className="text-lime-500">{">"}</span>
                 </div>
-                <motion.ul className="sm:flex hidden gap-10" initial={{ opacity: '0' }} animate={{opacity: 100}} transition={{ delay: 0.5}}>
-                    {
-                        navLinks.map((nav) => (
-                            <li key={nav.id} className={"p-7 uppercase tracking-widest h-[50px] w-[125px] items-center flex justify-center"} >
-                                <NavLink name={nav.title} link={nav.link}/>
-                            </li>
-                        ))
-                    }
+                <motion.ul
+                    className="sm:flex hidden gap-10"
+                    initial={{ opacity: "0" }}
+                    animate={{ opacity: 100 }}
+                    transition={{ delay: 0.5 }}
+                >
+                    {navLinks.map((nav) => (
+                        <li
+                            key={nav.id}
+                            className={
+                                "p-7 uppercase tracking-widest h-[50px] w-[125px] items-center flex justify-center"
+                            }
+                        >
+                            <NavLink name={nav.title} link={nav.link} />
+                        </li>
+                    ))}
                 </motion.ul>
                 {/* for mobile */}
                 <div className="sm:hidden flex m-10">
-                    <span className='' onClick={() => setToggle((prev) => !prev)}>
-                        {toggle ? <CgClose size={30} /> : <GiHamburgerMenu size={30} />}
+                    <span
+                        className=""
+                        onClick={() => setToggle((prev) => !prev)}
+                    >
+                        {toggle ? (
+                            <CgClose size={30} />
+                        ) : (
+                            <GiHamburgerMenu size={30} />
+                        )}
                     </span>
-                    <div className={`${toggle ? 'flex' : 'hidden'} absolute top-28 right-7 bg-white p-2 text-black z-10 rounded-xl`}>
-                        <ul className='flex gap-7 flex-col p-4'>
-                            {
-                                navLinks.map((nav) => (
-                                    <li key={nav.id}>
-                                        <a href={nav.link} className="uppercase tracking-widest">
-                                            {nav.title}
-                                        </a>
-                                    </li>
-                                ))
-                            }
+                    <div
+                        className={`${toggle ? "flex" : "hidden"} absolute top-28 right-7 bg-white p-2 text-black z-10 rounded-xl`}
+                    >
+                        <ul className="flex gap-7 flex-col p-4">
+                            {navLinks.map((nav) => (
+                                <li key={nav.id}>
+                                    <a
+                                        href={nav.link}
+                                        className="uppercase tracking-widest"
+                                    >
+                                        {nav.title}
+                                    </a>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
             </nav>
         </div>
-    )
-}
+    );
+};
